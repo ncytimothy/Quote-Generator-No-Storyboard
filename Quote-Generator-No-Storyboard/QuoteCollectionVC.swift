399598d371ddcff85e2195ea6a5a546d8c8fba24
Timothy Ng <timothyng2008@me.com>
2018-11-06 20:36:18 +0800

    - feat: 1. Set the Collection VC Title 2. Background Color Collection VC to White 3. QuoteCell class creation (override init(frame: CGSize) and override init(coder:)) 4. setupViews() in QuoteCell: addSubview() and addConstraints() 5. Declared nameLabel: UILabel using closure 6. alwaysBounceVertical !!!7. Setting label.translateAutoresizingMask = false 8. Implemented Collection VC Delegate Methods 9. Implemented the Collection View Flow Layout Delegate Method: sizeForRowAt: to set the cell to fill the entire view.frame.width and height of 50

--- Quote-Generator-No-Storyboard/QuoteCollectionVC.swift
+++ Quote-Generator-No-Storyboard/QuoteCollectionVC.swift
@@ -8,13 +8,83 @@
 
 import UIKit
 
-class QuoteCollectionVC: UICollectionViewController {
+let cellId = "quoteCell"
+
+class QuoteCollectionVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1. Add title
+        navigationItem.title = "Quote Creator!"
+        
+        // 2. Set background collection view background color
+        collectionView.backgroundColor = UIColor.white
+        
+        // 3. Register Cell to Collection View
+        collectionView.register(QuoteCell.self, forCellWithReuseIdentifier: cellId)
+        
+        // 4. Collection View Cells Vertical Bounce
+        collectionView.alwaysBounceVertical = true
     }
+    
+    
+    // 4. Collection View Delegate Methods
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let quoteCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return quoteCell
+    }
+    
+    // 5. Collection View Flow Layout Delegate Methods
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize = CGSize(width: view.frame.width, height: 50)
+        return cellSize
+    }
+    
 
+}
 
+class QuoteCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Little things make big things happen!"
+        label.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium)
+        // SUPER IMPORTANT: Disable Autoresizing Mask
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        
+        backgroundColor = UIColor.white
+        
+        addSubview(nameLabel)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(8)-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":nameLabel]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":nameLabel]))
+        
+        
+        
+    }
+    
+    
+    
 }
 
